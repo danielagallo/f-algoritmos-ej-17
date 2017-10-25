81ada08827e6aa7239bc51480a609db06c3bd811
Daniela Gallo <gc_danieladc@hotmail.com>
2017-10-25 17:22:44 -0500

    Ejercicio hecho en clases de encontrar el error

---
+++ Solucionar_error/src/solucionar_error/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solucionar_error;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Carrera c3 = new Carrera("distancia", "Sistemas");
+        System.out.println(c3);
+    }
+
+}
